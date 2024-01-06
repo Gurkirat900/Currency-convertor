9d143f79cb5ec7b45b8fbfc8390e6c05dda3eb9f
Gurkirat900 <gs1683676@gmail.com>
2024-01-07 01:01:31 +0530

    made excchange button working using currency api also added new msg div in html

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const dropDowns= document.querySelectorAll('.space-container select');
+const BASE_URL=
+"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
+
+const dropDowns= document.querySelectorAll('.space-container select');
+const btn= document.querySelector('.submit button');
+const fromCurr= document.querySelector(".from select");
+const toCurr= document.querySelector(".to select");
+const msg= document.querySelector(".msg");
 
 for(let select of dropDowns){
     for(currCode in countryList){
@@ -24,8 +31,31 @@ const updateFlag= (element)=>{
     let index= element.selectedIndex;
     let val= element.options[index].text;
     let flag= val.substring(0,val.length -1);
-    console.log(flag);
     let newsrc="https://flagsapi.com/"+flag+"/flat/64.png";
     let img = element.parentElement.querySelector("img");
     img.src=newsrc;
     }
+
+    const updateExchange= async ()=>{
+        let amount= document.querySelector('input');
+        let amountval= amount.value;
+        let URL= `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+        let response= await fetch(URL);
+        let data= await response.json();
+        let rate= data[toCurr.value.toLowerCase()];
+
+        let finalAmnt= rate*amountval;
+        msg.innerText=`${amountval} ${fromCurr.value} = ${finalAmnt} ${toCurr.value}`;
+    }
+
+    window.addEventListener("load",()=>{
+        updateExchange();
+    })
+
+    btn.addEventListener("click",async (ev)=>{
+        ev.preventDefault();
+       updateExchange();
+    })
+
+
+   
